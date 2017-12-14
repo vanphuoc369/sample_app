00a52d9603bf57d7306a0b2df0915e9052d163e6
vanphuoc369 <vanphuoc369@gmail.com>
2017-12-14 15:17:37 +0700

    Chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title page_title
-    base_title = t(:basetitle)
+    base_title = I18n.t :basetitle
     if page_title.blank?
       base_title
     else
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,6 @@ module ActiveSupport
   class TestCase
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
-    # Add more helper methods to be used by all tests here...
+    include ApplicationHelper
   end
 end
