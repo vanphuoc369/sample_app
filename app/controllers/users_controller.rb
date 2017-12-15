351a0558c14bf8d853ad6f9dd665385d26484b90
vanphuoc369 <vanphuoc369@gmail.com>
2017-12-15 14:45:10 +0700

    Chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      flash[:danger] = t(:err_find_user)
+      return redirect_to signup_path
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t(:notify_success)
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
 end
